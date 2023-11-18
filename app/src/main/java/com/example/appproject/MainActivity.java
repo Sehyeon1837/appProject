91542da804b79e1dca0d8033b9213772adeb7bf2
Seheyon <104759146+Seheyon@users.noreply.github.com>
2023-11-18 21:34:08 +0900

    make circles

--- app/src/main/java/com/example/appproject/MainActivity.java
+++ app/src/main/java/com/example/appproject/MainActivity.java
@@ -4,26 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
     private RadioGroup choiceYear;
     readfiles readfile;
+    mapMarker mapmarker;
     String inputYear;
-    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
         Resources res = getResources();
 
         choiceYear = findViewById(R.id.choiceyear);
-        text = findViewById(R.id.text);
 
-        readfile = new readfiles(text, res, this);
+        readfile = new readfiles(res, this);
         readfile.setYear("2017");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapmarker = new mapMarker(res, mapFragment, readfile);
 
         choiceYear.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -53,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                     inputYear = "2021";
                 }
                 readfile.setYear(inputYear);
+                mapmarker.changeYear();
             }
         });
     }
---
+++ app/src/main/java/com/example/appproject/mapMarker.java
@@ -0,0 +1,127 @@
+package com.example.appproject;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapMarker implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private readfiles readfile;
+    Resources res;
+    private SupportMapFragment mapFragment;
+    private String[] latlist, lonlist, area;
+    private String str, resultFac, temp;
+    private double tempUHI;
+    private MarkerOptions[] MarkerOptionsList;
+    private CircleOptions[] CircleOptionsList;
+    public mapMarker(Resources res, SupportMapFragment mapFragment, readfiles readfile) {
+        area = new String[19];
+        this.mapFragment = mapFragment;
+        mapFragment.getMapAsync(this);
+
+        this.res = res;
+        this.readfile = readfile;
+
+        latlist = res.getStringArray(R.array.lat_list);
+        lonlist = res.getStringArray(R.array.lon_list);
+        area = res.getStringArray(R.array.area_list);
+
+        MarkerOptionsList = new MarkerOptions[latlist.length];
+        CircleOptionsList = new CircleOptions[latlist.length];
+    }
+
+    public void onMapReady(final GoogleMap googleMap){
+        mMap = googleMap;
+        LatLng JEONJU = new LatLng(35.8412, 127.1318);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(JEONJU,16));
+
+        ChecktheMarkers();
+        setCircle();
+    }
+
+    public void changeYear() {
+        mMap.clear();
+        ChecktheMarkers();
+        setCircle();
+        //changeSnippetMarker();
+        //changeCircle();
+    }
+
+    private void ChecktheMarkers() {
+        for(int i = 0; i < latlist.length; i++) {
+            double lat = Double.parseDouble(latlist[i]);
+            double lon = Double.parseDouble(lonlist[i]);
+            LatLng LOCATION = new LatLng(lat, lon);
+
+            str = area[i];
+            resultFac = str.substring(str.lastIndexOf(".")+1);
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(LOCATION);
+            markerOptions.title(resultFac);
+
+            temp = readfile.getDatas(i);
+
+            if(temp == "")
+                temp = "해당 년도의 데이터가 존재하지 않습니다";
+
+            markerOptions.snippet(temp);
+
+            MarkerOptionsList[i] = markerOptions;
+            mMap.addMarker(markerOptions);
+        }
+    }
+
+    private void changeSnippetMarker() {
+        for(int i = 0; i < MarkerOptionsList.length; i++) {
+            temp = readfile.getDatas(i);
+            if(temp == "")
+                temp = "해당 년도의 데이터가 존재하지 않습니다";
+            MarkerOptionsList[i].snippet(temp);
+        }
+    }
+
+    private void setCircle() {
+        for(int i = 0; i < latlist.length; i++) {
+            double lat = Double.parseDouble(latlist[i]);
+            double lon = Double.parseDouble(lonlist[i]);
+            LatLng LOCATION = new LatLng(lat, lon);
+
+            CircleOptions circle1km = new CircleOptions().center(LOCATION)
+                    .radius(500)
+                    .fillColor(getColor(i))
+                    .strokeWidth(0f);
+
+            CircleOptionsList[i] = circle1km;
+
+            mMap.addCircle(circle1km);
+        }
+    }
+
+    private void changeCircle() {
+        for(int i = 0; i < CircleOptionsList.length; i++)
+            CircleOptionsList[i].fillColor(getColor(i));
+    }
+
+    private int getColor(int index) {
+        tempUHI = readfile.getUHITemp(index);
+        if(tempUHI >= 81)
+            return 0x80FF0000;
+        if(tempUHI >= 76 && tempUHI < 81)
+            return 0x80FF6600;
+        if(tempUHI >= 71 && tempUHI < 76)
+            return 0x80FF9900;
+
+        if(tempUHI == 0.0)
+            return 0x80CCCCCC;
+
+        return 0x80FFFF33;
+    }
+}
